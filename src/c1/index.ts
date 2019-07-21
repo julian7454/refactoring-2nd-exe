f198c795f72a86db64d76847941818e75093ed5e
julian <julian7454@gmail.com>
2019-07-21 14:12:45 +0800

    remove useless code

--- src/c1/index.ts
+++ src/c1/index.ts
@@ -37,24 +37,6 @@ function statement(invoice, plays) {
   for (let perf of invoice.performances) {
     let thisAmount = amountFor(perf);
 
-    switch (playFor(perf).type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${playFor(perf).type}`);
-    }
-
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
