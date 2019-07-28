f0b2119dbb090cd4456b9f523fc5342a1d4dbda0
julian <julian7454@gmail.com>
2019-07-28 12:09:52 +0800

    refactor(c1): extract render text function

--- src/c1/index.ts
+++ src/c1/index.ts
@@ -25,6 +25,10 @@ const invoices = [
 ];
 
 function statement(invoice, plays) {
+  return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
